c0f022bbefac014d7238b3d526fdb80058d6db55
AngryCow1 <32710495+AngryCow1@users.noreply.github.com>
2017-10-11 16:46:05 +0200

    and
    asd

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'OqbufcovwgvwBviMJvQnyx1Ve',
+    consumer_secret:      'kL13Z0KXPm1oovzpRe9f3kgJVqgrrzQzZgdmG5MhbJNed5vSEb',
+    access_token:         '918101102490464256-444baHbk3ppsSG9e8hjwsauWrp05Rpy',
+    access_token_secret:  'GcMCy0svWrW6HsRZEZzeAavFeY27yXeDJyHfeDGrb8ii6',
+    }
